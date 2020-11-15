62c1a7e4cbfdd9b00124bc1e1ddca142fe15ac25
Teo Winkler <teo.winkler@gmail.com>
2020-11-15 12:37:15 +0100

    add base scss files

---
+++ portfolio/webpack.config.js
@@ -0,0 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              // you can also read from a file, e.g. `variables.scss`
+              // use `prependData` here if sass-loader version = 8, or
+              // `data` if sass-loader version < 8
+              prependData: `$color: red;`
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
+}
